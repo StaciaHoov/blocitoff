e5312e3dbcba9bda5250b14b398048fea30cb756
staciahoov <staciaray@roadrunner.com>
2015-08-16 18:39:12 +0000

    added ajax for delete checkmark

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,9 @@ class ItemsController < ApplicationController
 
   def create
     @items = current_user.items
-    @item = current_user.items.create(params.require(:item).permit(:name))
+    @item = current_user.items.create(items_params)
     @item.expires_at = Time.now + 7.days
+    @user = current_user
     
     if @item.save
       flash[:notice] = "Task added to your To Do list."
@@ -12,6 +13,31 @@ class ItemsController < ApplicationController
       flash[:error] = "Error saving task. Please try again."
     end
     redirect_to user_path(current_user)
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    @user = current_user
+    @item = current_user.items.find(params[:id])
+    
+    if @item.delete
+      flash[:notice] = "Task completed and removed from your list."
+    else
+      flash[:error] = "There was an error marking your task as complete."
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+    
+  
+  
+  private
+  
+  def items_params
+    (params.require(:item).permit(:name))
   end
 
 end
